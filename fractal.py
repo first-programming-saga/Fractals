19214643ae3f9cac6d30553435e03b3ce0be3b4d
tadaki <tada195908@gmail.com>
2023-12-16 17:37:35 +0900

    20231216

--- fractal.py
+++ fractal.py
@@ -1,6 +1,12 @@
 import numpy as np
+import numpy.typing as npt
 import math
 import matplotlib.patches as pt
+from typing import NamedTuple
+
+class Point(NamedTuple):
+    x:float
+    y:float
 
 class AF:
     """
@@ -8,12 +14,12 @@ class AF:
     """
     def __init__(self, r:float, s:float, phi:float, psi:float,
     e:float, f:float):
-        self.__m = np.array([
+        self.__m: npt.NDArray = np.array([
                 [r * math.cos(phi), -s * math.sin(psi)],
                 [r * math.sin(phi), s * math.cos(psi)]])
-        self.__t = np.array([[e], [f]])
+        self.__t: npt.NDArray = np.array([[e], [f]])
 
-    def trans(self, v:tuple[float, float])->tuple[float,float]:
+    def trans(self, v:Point)->Point:
         """
         Translate one point
 
@@ -25,11 +31,11 @@ class AF:
         ----------
         transformed two dimensional coordinate
         """
-        vv = np.array([[v[0]], [v[1]]])
+        vv: npt.NDArray = np.array([[v.x], [v.y]])
         r = self.__m @ vv + self.__t
-        return r[0][0], r[1][0]
+        return Point(r[0][0], r[1][0])
 
-    def transList(self,vl:list[tuple[float,float]]):
+    def transList(self,v_list:list[Point]) -> list[Point]:
         """
         Translate list of points
 
@@ -41,8 +47,8 @@ class AF:
         ----------
         list of transformed two dimensional coordinates
         """
-        result = list()
-        for v in vl:
+        result:list[Point] = list()
+        for v in v_list:
             result.append(self.trans(v))
         return result        
 
@@ -50,10 +56,10 @@ class Fractal:
     """
     Fractal class
     """
-    def __init__(self,afList:list[AF], xy = [(0, 0), (0, 1), (1, 1), (1, 0)]
-):
-        self.__afList = afList
-        self.__shapes = [xy]
+    default_xy =  [Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)]
+    def __init__(self,afList:list[AF], xy = default_xy):
+        self.__afList: list[AF] = afList
+        self.__shapes:list[list[Point]] = [xy]
 
     def iterate(self):
         """
@@ -72,8 +78,15 @@ class Fractal:
         """
         sp = list()
         for xy in self.__shapes:
-            sp.append(pt.Polygon(xy, fill = True, color = 'b'))
+            sp.append(pt.Polygon(Fractal._p2a(xy), fill = True, color = 'b'))
         return sp
+
+    @staticmethod
+    def _p2a(data:list[Point]) -> list[list[float]]:
+        result:list[list[float]]= list()
+        for p in data:
+            result.append([p.x,p.y])
+        return result
 
     def getMap(self)->list[pt.Polygon]:
         """
@@ -82,14 +95,13 @@ class Fractal:
         sp = list()
         for af in self.__afList:
             sp.append(pt.Polygon(
-                af.transList([(0, 0), (0, 1), (1, 1), (1, 0)]),fill =True, color='c',ec='black'))
+                Fractal._p2a(af.transList(Fractal.default_xy)),
+                fill =True, color='c',ec='black'))
             sp.append(pt.Polygon(
-                af.transList([(0.1,0.1),(0.2,0.1),(0.1,0.3)]), fill = True, color='r'))
+                Fractal._p2a(af.transList([Point(0.1,0.1),Point(0.2,0.1),Point(0.1,0.3)])), 
+                fill = True, color='r'))
         return sp
 
     @property
-    def shapes(self):
-        pass
-    @shapes.getter
-    def shapes(self):
+    def shapes(self) -> list[list[Point]]:
         return self.__shapes
